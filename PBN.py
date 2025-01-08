f6bb0cb5ade48a9e62d5c0dff9f9d7858966aa41
MChihaya <morita-chihaya070@g.ecc.u-tokyo.ac.jp>
2025-01-09 02:25:17 +0900

    pbn 完成

--- PBN.py
+++ PBN.py
@@ -1,26 +1,145 @@
 import random
 import time
-def is_constfunc(a, n) -> bool:
-    x = a[0]
-    for i in range(n):
-        if a[i] != x:
-            return False
-    return True
 
-def encode2to10(state, node) -> int:
-    res = 0
-    for i in range(node):
-        res = res * 2
-    return res
+class PBN:
 
-def decode10to2(state, num10d, node) -> None:
-    n = 0
-    div = 2 ** (node - 1)
+    def __init__(self, nodenum, degree, is_thereconst, prob):
+        self.nodenum = nodenum
+        self.node = [self.Node(degree, prob) for _ in range(nodenum)]
+        self.degree = degree
+        self.is_thereconst = is_thereconst
+        self.prob = prob
+        self.check = [0 for _ in range(2 ** nodenum)]
+        self.count = 1
+        self.count2 = 0
+    
+    class Node:
+        def __init__(self, degree, prob):
+            self.degree = degree
+            self.func = [None for _ in range(2 ** degree)]
+            self.topology = []
+            self.prob = prob
+            self.state = None
+            self.oldstate = None
+        def set_func(self, is_thereconst) -> None:
+            num2d = 2 ** self.degree
+            for i in range(num2d):
+                if (random.random() < self.prob):
+                    self.func[i] = 1
+                else:
+                    self.func[i] = 0
+                
+                if is_thereconst == 0 and self.is_constfunc(self.func, num2d) == True:
+                    i -= 1
+        def get_nextstate(self, input) -> int:
+            return self.func[input]
+        
+        def is_constfunc(self) -> bool:
+            x = self.func[0]
+            for i in range(2 ** self.degree):
+                if self.func[i] != x:
+                    return False
+            return True
+        def set_topology(self, nodenum) -> None:
+            while len(self.topology) < self.degree:
+                tmp = random.randint(0, nodenum - 1)
+                is_overlap = False
+                for topology in self.topology:
+                    if topology == tmp:
+                        is_overlap = True
+                        break
+                if is_overlap == False:
+                    self.topology.append(tmp)
+    def encode2to10(self) -> int:
+        res = 0
+        for node in self.node:
+            res = res * 2
+            res += node.state
+        return res
+    
+    def decode10to2(self, num10d) -> None:
+        n = 0
+        div = 2 ** (self.nodenum - 1)
 
-    for n in range(node):
-        state[n] = num10d // div
-        num10d = num10d % div
-        div = div // 2
+        for n in range(self.nodenum):
+            self.node[n].oldstate = num10d // div
+            num10d = num10d % div
+            div = div // 2
+    
+    def decide_func(self) -> None:
+        n = 2 ** self.degree
+        for i in range(self.nodenum):
+            self.node[i].set_func(self.is_thereconst)
+    
+    def print_bool_table(self) -> None:
+        print(*[i + 1 for i in range(self.degree)], "|", *[chr(ord('A') + i) for i in range(self.nodenum)])
+        for num2d in range(2 ** self.degree):
+            if self.degree >= 4:
+                print(num2d//8, (num2d%8) // 4, (num2d%4) // 2, num2d%2, "|", end="")
+            elif self.degree == 3:
+                print(num2d//4, (num2d%4) // 2, num2d%2, "|", end="")
+            elif self.degree == 2:
+                print(num2d//2, num2d%2, "|", end="")
+        
+            for node in self.node:
+                print(" " + str(node.func[num2d]), end="")
+            print("")
+    
+    def decide_topology(self) -> None:
+        for i in range(self.nodenum):
+            self.node[i].set_topology(self.nodenum)
+    
+    def print_topology(self) -> None:
+        print("  |", end="")
+        for i in range(self.degree):
+            print(" " + str(i+1), end="")
+        print("")
+        for i in range(self.nodenum):
+            print(chr(ord('A') + i) + " |", end="")
+            for j in range(self.degree):
+                print(" " + chr(ord('A') + self.node[i].topology[j]), end="")
+            print("")
+    
+    def run(self) -> None:
+        node_all_pattern_num = 2 ** self.nodenum
+
+        for node_pattern in range(node_all_pattern_num):
+            stateint = node_pattern
+            self.decode10to2(node_pattern)
+
+            if self.check[node_pattern] != 0:
+                continue
+
+            print(f"({self.count}) ", end="")
+            while True:
+                if self.check[stateint] != 0:
+                    break
+
+                self.check[stateint] = self.count
+                for j in range(self.nodenum):
+                    print(self.node[j].oldstate, end="")
+                print("->", end="")
+
+                for node in self.node:
+                    funcnum = 0
+                    for k in range(self.degree):
+                        funcnum = funcnum * 2 + self.node[node.topology[k]].oldstate
+                    node.state = node.get_nextstate(funcnum)
+
+                for node in self.node:
+                    node.oldstate = node.state
+
+                stateint = self.encode2to10()
+
+            for node in self.node:
+                print(node.oldstate, end="")
+            print(f"({self.check[stateint]})")
+
+            if self.check[stateint] == self.count:
+                self.count2 += 1
+            self.count += 1
+
+        print("\nアトラクタ数:", self.count2)
 
 def main():
     random.seed(time.time())
@@ -28,6 +147,7 @@ def main():
     degree = None
     is_thereconst = None
     prob = None
+    
     while True:
         nodenum = int(input("ノードの数を入力してください(4～7)"))
         if nodenum >= 4 and nodenum <= 7:
@@ -48,104 +168,18 @@ def main():
         if prob >= 0.0 and prob <= 1.0:
             break
 
+    pbn = PBN(nodenum, degree, is_thereconst, prob)
     print("ブール関数をランダムに決定します。")
-    n = 2 ** degree
-    func = [[None for _ in range(n)] for _ in range(nodenum)]
-    for i in range(nodenum):
-        for j in range(n):
-            if (random.random() < prob):
-                func[i][j] = 1
-            else:
-                func[i][j] = 0
-        
-        if is_thereconst == 0 and is_constfunc(func[i], n):
-            i -= 1
+    pbn.decide_func()
     
-    print(*[i + 1 for i in range(degree)], "|", *[chr(ord('A') + i) for i in range(nodenum)])
+    pbn.print_bool_table()
 
-    for i in range(n):
-        if degree >= 4:
-            print(i//8, (i%8) // 4, (i%4) // 2, i%2, "|", end="")
-        elif degree == 3:
-            print(i//4, (i%4) // 2, i%2, "|", end="")
-        elif degree == 2:
-            print(i//2, i%2, "|", end="")
-    
-        for j in range(nodenum):
-            print(" " + str(func[j][i]), end="")
-        print("")
-    print("それぞれのノードの入力がどのノードから来るのかを表示(トポロジーの決定)");
-    print("  |")
-    for i in range(degree):
-        print(" " +str(i+1), end="")
-    print("")
-
-    topology = [[None for _ in range(4)] for _ in range(7)]
-    for i in range(nodenum):
-        print(chr(ord('A') + i)+ " |", end="")
-        for j in range(degree):
-            while True:
-                tmp = random.randint(0, nodenum - 1)
-                is_overlap = False
-                for k in range(j):
-                    if topology[i][k] == tmp:
-                        is_overlap = True
-                        break
-                if is_overlap == False:
-                    topology[i][j] = tmp
-                    print(" %c" % chr(ord('A')+tmp), end="")
-                    break
-        print("")
+    print("それぞれのノードの入力がどのノードから来るのかを表示(トポロジーの決定)")
+    pbn.decide_topology()
+    pbn.print_topology()
     
     print("ネットワークを動作させます\n")
-    n = 2 ** nodenum
-    state = [0 for _ in range(nodenum)]
-    oldstate = [0 for _ in range(nodenum)]
-    check = [0 for _ in range(n)]
-    count = 1
-    count2 = 0
-
-    for i in range(n):
-        stateint = i
-        decode10to2(oldstate, i, nodenum)
-
-        if check[stateint] != 0:
-            continue
-
-        print(f"({count}) ", end="")
-        while True:
-            if check[stateint] != 0:
-                break
-
-            check[stateint] = count
-            for j in range(nodenum):
-                print(oldstate[j], end="")
-            print("->", end="")
-
-            for j in range(nodenum):
-                funcnum = 0
-                for k in range(degree):
-                    funcnum = funcnum * 2 + oldstate[topology[j][k]]
-                state[j] = func[j][int(funcnum)]
-
-            for j in range(nodenum):
-                oldstate[j] = state[j]
-
-            stateint = encode2to10(state, nodenum)
-
-        for j in range(nodenum):
-            print(oldstate[j], end="")
-        print(f"({check[stateint]})")
-
-        if check[stateint] == count:
-            count2 += 1
-        count += 1
-
-    print("\nアトラクタ数:", count2)
-
-    
-    
-    
+    pbn.run()
 
 
 if __name__ == "__main__":
