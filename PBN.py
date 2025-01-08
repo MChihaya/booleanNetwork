1c655846c26f3d22e3d9739cf736a9b28c879ba9
Morita Chihaya <morita-chihaya070@g.ecc.u-tokyo.ac.jp>
2025-01-08 13:25:44 +0900

    PBN 少し

--- PBN.py
+++ PBN.py
@@ -47,6 +47,35 @@ def main():
         prob = float(input("確率を入力してください(0.0～1.0)"))
         if prob >= 0.0 and prob <= 1.0:
             break
+
+    print("ブール関数をランダムに決定します。")
+    n = n ** degree
+    func = [[None for _ in range(n)] for _ in range(nodenum)]
+    for i in range(nodenum):
+        for j in range(n):
+            if (random.random() < prob):
+                func[i][j] = 1
+            else:
+                func[i][j] = 0
+        
+        if is_thereconst == 0 and is_constfunc(func[i], n):
+            i -= 1
+    
+    print(*[i + 1 for i in range(degree)], "|", *[chr(ord('A') + i) for i in range(nodenum)])
+
+    for i in range(n):
+        if degree >= 4:
+            print(i//8, (i%8) // 4, (i%4) // 2, i%2, "|", end="")
+        elif degree == 3:
+            print(i//4, (i%4) // 2, i%2, "|", end="")
+        elif degree == 2:
+            print(i//2, i%2, "|", end="")
+    
+        for j in range(nodenum):
+            print(" " + str(func[j][i]), end="")
+
+
+    
     
     
 
